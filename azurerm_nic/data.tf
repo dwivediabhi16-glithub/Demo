d0f170405cda7b15ec17b323a188e926d7de7937
abhishek dwivedi <dwivedi.abhi16@gmail.com>
2026-08-03 21:32:07 +0530

    add

---
+++ azurerm_nic/data.tf
@@ -0,0 +1,11 @@
+data "azurerm_subnet" "subnet_id" {
+    for_each = var.network_interface
+  name                 = each.value.subnet_name
+  virtual_network_name = each.value.virtual_network_name
+  resource_group_name  = each.value.resource_group_name
+}
+data "azurerm_public_ip" "pip_id" {
+    for_each = var.network_interface
+  name                = each.value.public_ip_name
+  resource_group_name = each.value.resource_group_name
+}
